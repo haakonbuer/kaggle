275c6e671e4f8de5810df610ad1f634daf1e94b7
HaakonB <haakonbuer@gmail.com>
2015-11-30 22:43:07 +0000

    end of the day

--- digits/digitizer.py
+++ digits/digitizer.py
@@ -1,8 +1,35 @@
+import csv
+
 __author__ = 'haakon'
 
 
-def main():
+def parse_training_data(data):
+    info = []
+    for row in data:
+        info.append(row)
+
+    return info
+
+
+def parse_test_data(data):
     pass
+
+
+def main():
+
+    train_data = []
+    with open('train.csv', 'rb') as csvfile:
+        print "Opened file"
+        fr = csv.reader(csvfile)
+        for row in fr:
+            train_data.append(row)
+
+    #test = csv.reader('test.csv')
+
+    #train_data = parse_training_data(train)
+    #test_data = parse_test_data(test)
+
+    print len(train_data)
 
 
 if __name__ == "__main__":
